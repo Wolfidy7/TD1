1af2dd7ccf392744142aa347946a96382f5a980a
Sherif Makalou <makaloucherif@gmail.com>
2022-09-24 15:01:06 +0200

    exo 2

---
+++ v4/generator/generator.c
@@ -0,0 +1,52 @@
+#include "generator.h"
+#include <stdarg.h>
+#define INITIAL_VALUE 1
+#include <stdio.h>
+#define INITIAL_STEP 1
+
+
+#define RECUPERER(parametres, debut, variable) \
+	va_start(parametres, debut); \
+	variable= va_arg(parametres, int); \
+	va_end(parametres);
+
+
+int generator_generate(message m, ...)
+{
+	static int init_val = INITIAL_VALUE;
+	static int current_val = INITIAL_VALUE;
+	static int step = INITIAL_STEP;
+	va_list parametres;
+
+	switch(m)
+	{
+	case Premier:
+		//RECUPERER(parametres, m, init_val);
+		va_start(parametres, m);
+		init_val = va_arg(parametres, int);
+		va_end(parametres);
+		break;
+
+	case Pas:
+		///RECUPERER(parametres, m, step);
+		va_start(parametres, m);
+		step = va_arg(parametres, int);
+		va_end(parametres);
+		break;
+
+	case Debut:
+		current_val = init_val;
+		break;
+
+	case Suivant:
+	{
+		int n = current_val;
+		current_val += step;
+		return n;
+	}
+	default:
+		printf("problemo !!!");
+		break;
+	}
+	return 0;
+}
